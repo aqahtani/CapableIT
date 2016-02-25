6066c556fb303731f68afe8f9e870f5c34071ebc
aqahtani <aq@knowledge-passion.com>
2016-02-25 14:30:57 +0300

    Improved: email tests for all email views

--- app/routes/emails.js
+++ app/routes/emails.js
@@ -26,7 +26,7 @@ module.exports = {
 		
 		var newEnquiry = new Enquiry.model({
 			name: { first: 'Test', last: 'User' },
-			email: 'contact@my-keystone.com',
+			email: 'contact@' + process.env.DOMAIN,
 			phone: '+61 2 1234 5678',
 			enquiryType: 'message',
 			message: { md: 'Nice enquiry notification.' }
@@ -38,6 +38,58 @@ module.exports = {
 			enquiry_url: '/keystone/enquiries/'
 		});
 		
-	}
+    },
+
+    'reset-password' : function (req, res, callback) {
+        
+        // To test reset-password we create a dummy user that
+        // is not saved to the database, but passed to the template.
+        
+        var User = keystone.list('User');
+        
+        var newUser = new User.model({
+            name: { first: 'Test', last: 'User' },
+            email: 'user@test.com',
+            resetPasswordKey: 'DummYReSETKeY'
+        });
+        
+        callback(null, {
+            user: newUser,
+            subject: '[CapableIT] Reset your password',
+            to: newUser.email,
+            from: {
+                name: 'CapableIT',
+                email: 'no-reply@' + process.env.DOMAIN
+            }
+        });
+		
+    },
+
+    'verify-email' : function (req, res, callback) {
+        
+        // To test verify-email we create a dummy user that
+        // is not saved to the database, but passed to the template.
+        
+        var User = keystone.list('User');
+        
+        var newUser = new User.model({
+            name: { first: 'Test', last: 'User' },
+            email: 'user@test.com',
+            emailVerificationKey: 'DummYReSETKeY'
+        });
+        
+        callback(null, {
+            user: newUser,
+            brand: 'CapableIT',
+            subject: '[CapableIT] Reset your password',
+            to: newUser.email,
+            from: {
+                name: 'CapableIT',
+                email: 'no-reply@' + process.env.DOMAIN
+            }
+        });
+		
+    }
+
 	
 };
